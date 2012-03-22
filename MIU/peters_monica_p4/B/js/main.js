8d8c0f6be66e97f37f0f657b27cdc48145c6af8f
Monica <monigarr@yahoo.com>
2012-03-21 23:21:38 -0400

    App B is ready to submit for grade

--- MIU/peters_monica_p4/B/js/main.js
+++ MIU/peters_monica_p4/B/js/main.js
@@ -41,8 +41,9 @@
 // Wait until DOM is ready
 window.addEventListener("DOMContentLoaded", function()
 {
+
 	// getElementById Function
-	function noDollarSign(x)
+	function momo(x)
 	{
 		var theElement = document.getElementById(x);
 		return theElement;
@@ -53,12 +54,9 @@ window.addEventListener("DOMContentLoaded", function()
 	{
 		//formTag is an array of all form tags
 		var formTag = document.getElementsByTagName("form"),
-			selectLi = noDollarSign("mtype"),
+			selectLi = momo("select"),
 			makeSelect = document.createElement("select");
-			makeSelect.setAttribute("class", "required");
 			makeSelect.setAttribute("id", "mtype");
-			makeSelect.setAttribute("name", "mtype");
-			makeSelect.setAttribute("data-native-menu", "false");
 		//populate with options
 		for(var i=0, j=mediaGroups.length; i<j; i++) 
 		{
@@ -87,43 +85,27 @@ window.addEventListener("DOMContentLoaded", function()
 		}
 	}
 	
-	
 	//Turn nav links off / on
 	function toggleControls(n)
 	{
-		/*
 		switch(n)
 		{
 			case "on":
-				noDollarSign("projectForm").style.display = "block";
-				noDollarSign("clear").style.display = "inline";
-				noDollarSign("displayLink").style.display = "inline";
-				noDollarSign("addNew").style.display = "inline";
-				noDollarSign("displayIOSLink").style.display = "inline";
-				noDollarSign("displayAndroidLink").style.display = "inline";
-				noDollarSign("displayHtml5Link").style.display = "inline";
-				noDollarSign("displayWordpressLink").style.display = "inline";
-				noDollarSign("displayGraphicLink").style.display = "inline";
-				noDollarSign("displayAuthorLink").style.display = "inline";
+				momo("projectForm").style.display = "none";
+				momo("clear").style.display = "inline";
+				momo("displayLink").style.display = "none";
+				momo("addNew").style.display = "inline";
 				break;
 			case "off":
-				noDollarSign("projectForm").style.display = "block";
-				noDollarSign("clear").style.display = "inline";
-				noDollarSign("displayLink").style.display = "inline";
-				//noDollarSign("displayLink").listview("refresh");
-				noDollarSign("addNew").style.display = "inline";
-				noDollarSign("items").style.display = "inline";
-				noDollarSign("displayIOSLink").style.display = "inline";
-				noDollarSign("displayAndroidLink").style.display = "inline";
-				noDollarSign("displayHtml5Link").style.display = "inline";
-				noDollarSign("displayWordpressLink").style.display = "inline";
-				noDollarSign("displayGraphicLink").style.display = "inline";
-				noDollarSign("displayAuthorLink").style.display = "inline";
+				momo("projectForm").style.display = "block";
+				momo("clear").style.display = "inline";
+				momo("displayLink").style.display = "inline";
+				momo("addNew").style.display = "none";
+				momo("items").style.display = "none";
 				break;
 			default:
-				return true;
+				return false;
 		}
-		*/
 	}
 	
 	function saveMedia(key)
@@ -137,13 +119,12 @@ window.addEventListener("DOMContentLoaded", function()
 			//alert(localStorage.key(0));
 			var id = Math.floor(Math.random()*10000001);
 		}
-		
-		/*//Remove Weird Data that creates keys for file directories
+		//Remove Weird Data that creates keys for file directories
 		else if(key === "A-Z" || "a-z")
 		{
 			//delete weird data and move on
 			localStorage.removeItem(this.key);
-		}*/
+		}
 		else
 		{
 			//set the id to existing key we're editing
@@ -158,18 +139,19 @@ window.addEventListener("DOMContentLoaded", function()
 			//Gather up all our form field values and store in object.
 			//Object properties contain array with form label and input value
 			var item 			= {};
-				item.mtype 		= ["Project Type:",noDollarSign("mtype").value];
-				item.mgraphic   = ["Project Screenshot:",noDollarSign("mgraphic").value];
-				item.mname 		= ["Project Name:",noDollarSign("mname").value];
-				item.mdate  	= ["Project Date:",noDollarSign("mdate").value];
-				item.mrating 	= ["Project Rating:",noDollarSign("mrating").value];
+				item.mtype 		= ["Project Type:",momo("mtype").value];
+				item.mgraphic   = ["Project Screenshot:",momo("mgraphic").value];
+				item.mname 		= ["Project Name:",momo("mname").value];
+				item.mdate  	= ["Project Date:",momo("mdate").value];
+				item.mrating 	= ["Project Rating:",momo("mrating").value];
 				//radio button
 				item.mtopics 	= ["Project Incentive:",mtopicsValue];
-				item.mtags		= ["Project Tags:",noDollarSign("mtags").value];
-				item.mcomments	= ["Project Notes:",noDollarSign("mcomments").value];
+				item.mtags		= ["Project Tags:",momo("mtags").value];
+				item.mcomments	= ["Project Notes:",momo("mcomments").value];
 			//Save Data to Local Storage: Use Stringify to convert our object to a string
 			//json.org
 			localStorage.setItem(id, JSON.stringify(item));
+			alert("Project Saved");
 	}
 	
 	//Auto Populate local storage
@@ -197,68 +179,51 @@ window.addEventListener("DOMContentLoaded", function()
 			autoFillData();
 		}
 		
-
-		
 		//Write Data from Local Storage to the Browswer.
 		var makeDiv = document.createElement("div");
 		makeDiv.setAttribute("id","items");
 		makeDiv.setAttribute("data-role", "content");
-		makeDiv.setAttribute("data-theme", "d");
-		
-		var makeDivPrimary = document.createElement("div");
-		makeDivPrimary.setAttribute("class", "content-primary");
-
+		makeDiv.setAttribute("data-add-back-btn", "true");
 		var makeList = document.createElement("ul");
-		makeList.setAttribute("id", "one");
 		makeList.setAttribute("data-role", "listview");
-		makeList.setAttribute("data-filter", "true");
+		makeList.setAttribute("data-theme", "d");
 		makeList.setAttribute("data-inset", "true");
-		//$('#items').listview('refresh');	
-		//makeList.listview("refresh");
+		makeList.setAttribute("data-filter", "true");
 		
-		makeDiv.appendChild(makeDivPrimary);
-		makeDivPrimary.appendChild(makeList);
+		makeDiv.appendChild(makeList);
 		document.body.appendChild(makeDiv);
-		noDollarSign("items").style.display = "black";
-			
+		momo("items").style.display = "black";
+		
 		for(var i=0, len=localStorage.length; i<len; i++)
 		{
+			//CHANGE TO JQUERYMOBILE GRID VIEW
+			//http://jquerymobile.com/demos/1.1.0-rc.1/docs/content/content-grids.html
 			var makeli = document.createElement("li");
-			makeli.setAttribute("id", "two");
-			
+			makeli.setAttribute("id", "one");
 			var linksLi = document.createElement("div");
-			linksLi.setAttribute("id", "three");
-			
+			linksLi.setAttribute("id", "two");
 			makeList.appendChild(makeli);
-			
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			//convert string back to object so it won't be one long string
 			var obj = JSON.parse(value);
-			var makeSubList = document.createElement("a");
-			makeSubList.setAttribute("href", "#");
-			makeSubList.setAttribute("id", "four");
-
+			var makeSubList = document.createElement("p");
 			makeli.appendChild(makeSubList);
-			
-			//Add Icon for each Project Type
-			//getImage(obj.mtype[1], makeSubList);
+
 			//Add Graphic for each Project Name
 			getProjectGraphic(obj.mgraphic[1], makeSubList);
 			
-			//build p to hold all values for each project
-			var makeSubli = document.createElement("p");
-			makeSubli.setAttribute("id", "five");
-			makeSubList.appendChild(makeSubli);
-				
 			for(var n in obj)
 			{
+				var makeSubli = document.createElement("div");
+				makeSubList.appendChild(makeSubli);
 				//0 is label, 1 is the value
-				//var optSubText = obj[n][0] + " " + obj[n][1];
-				var optSubText = obj[n][1];
+				var optSubText = obj[n][0] + " " + obj[n][1];
 				makeSubli.innerHTML = optSubText;
 				makeSubli.appendChild(linksLi);
 			}
+			//Add Icon for each Project Type
+			getImage(obj.mtype[1], makeSubList);
 			//add edit and delete button from function
 			//for each item in local storage.
 			makeItemLinks(localStorage.key(i), linksLi);
@@ -269,7 +234,7 @@ window.addEventListener("DOMContentLoaded", function()
 	function getImage(mediaType, makeSubList)
 	{
 		var imageLi = document.createElement("div");
-		imageLi.setAttribute("align", "left");		
+		imageLi.setAttribute("align", "left");
 		makeSubList.appendChild(imageLi);
 		var newImg = document.createElement("img");
 		var setSrc = newImg.setAttribute("src", "images/" + mediaType + ".jpg");
@@ -277,11 +242,11 @@ window.addEventListener("DOMContentLoaded", function()
 		imageLi.appendChild(newImg);
 	}
 	
-	
 	//Get graphic url for project.
 	function getProjectGraphic(projectName, makeSubList)
 	{
 		var projectGraphicLi = document.createElement("div");
+		projectGraphicLi.setAttribute("align", "right");
 		makeSubList.appendChild(projectGraphicLi);
 		var newImg = document.createElement("img");
 		var setSrc = newImg.setAttribute("src", projectName);
@@ -331,15 +296,16 @@ window.addEventListener("DOMContentLoaded", function()
 		var value = localStorage.getItem(this.key);
 		var item = JSON.parse(value);
 		
-		//show form to edit item
-		//toggleControls("off");
+		//show form to edit iem
+		toggleControls("off");
 		
 		//populate form fields with current local storage values
 		//1 is value, 0 is label
-		noDollarSign("mtype").value = item.mtype[1];
-		noDollarSign("mdate").value = item.mdate[1];
-		noDollarSign("mname").value = item.mname[1];
-		noDollarSign("mrating").value = item.mrating[1];
+		momo("mtype").value = item.mtype[1];
+		momo("mgraphic").value = item.mgraphic[1];
+		momo("mname").value = item.mname[1];
+		momo("mdate").value = item.mdate[1];
+		momo("mrating").value = item.mrating[1];
 		// handle radio buttons
 		var radios = document.forms[0].mtopics;
 		for(var i=0; i<radios.length; i++)
@@ -347,12 +313,10 @@ window.addEventListener("DOMContentLoaded", function()
 			if(radios(i).value === "school" && item.mtopics(1) === "school")
 			{
 				radios(i).setAttribute("checked", "checked");
-			}
-			else if(radios(i).value === "work" && item.mtopics(1) === "work")
+			}else if(radios(i).value === "work" && item.mtopics(1) === "work")
 			{
 				radios(i).setAttribute("checked", "checked");
-			}
-			else if(radios(i).value === "inspiration" && item.mtopics(1) === "inspiration")
+			}else if(radios(i).value === "inspiration" && item.mtopics(1) === "inspiration")
 			{
 				radios(i).setAttribute("checked", "checked");
 			}
@@ -361,20 +325,17 @@ window.addEventListener("DOMContentLoaded", function()
 		// handle yes / no check box
 		if(obj.favorite(1) == "Yes")
 		{
-			noDollarSign("fav").setAttributes("checked", "checked");
+			momo("fav").setAttributes("checked", "checked");
 		}
 		*/
-		noDollarSign("mtags").value = item.mtags[1];
-		noDollarSign("mgraphic").value = item.mgraphic[1];
-		noDollarSign("mcomments").value = item.mcomments[1];
+		momo("mtags").value = item.mtags[1];
+		momo("mcomments").value = item.mcomments[1];
 		
 		// Remove the initial listener from the input 'save project' button
 		save.removeEventListener("click", saveMedia);
-		
-		// Change Submit button value to say Edit Button
-		noDollarSign("submit").value = "Edit Project";
-		var editSubmit = noDollarSign("submit");
-		
+		// Change Submit button value to day Edit Button
+		momo("submit").value = "Edit Project";
+		var editSubmit = momo("submit");
 		// Save the key value established in this Function as a property of the editSubmit event
 		// so we can use the value when we save the data we edited.
 		editSubmit.addEventListener("click", validate);
@@ -413,13 +374,12 @@ window.addEventListener("DOMContentLoaded", function()
 		}
 	}
 	
-	
 	function validate(e)
 	{
 		//Define elements we want to check
-		var getMtype = noDollarSign("mtype");
-		var getMname = noDollarSign("mname");
-		var getMdate = noDollarSign("mdate");
+		var getMtype = momo("mtype");
+		var getMname = momo("mname");
+		var getMdate = momo("mdate");
 		
 		//Reset error messages
 		errMsg.innerHTML = "";
@@ -433,25 +393,26 @@ window.addEventListener("DOMContentLoaded", function()
 		
 		if(getMtype.value === "-- Choose Project Type--")
 		{
-			var MtypeError = "Choose Project Type";
-			getMtype.style.border = "1px solid yellow";
-			messageAry.push(MtypeError);
+			alert("Enter Project Type");
+			window.location.reload();
 		}
 		
 		// Project Name Validation
 		if(getMname.value === "")
 		{
-			var MnameError = "Enter Project Name";
-			getMname.style.border = "1px solid yellow";
+			var MnameError = "Enter Project Name.";
+			getMname.style.border = "1px solid #FFFF99";
 			messageAry.push(MnameError);
+			//alert("Enter Project Name");
+			//window.location.reload();
 		}
 		
 		// Project Date Validation
 		if(getMdate.value === "")
 		{
-			var MdateError = "Enter Project Date";
-			getMdate.style.border = "1px solid yellow";
-			messageAry.push(MdateError);
+			var mdateError = "Enter Project Date";
+			getMdate.style.border = "1px solid red";
+			messageAry.push(mdateError);
 		}
 		
 		//if errors, show them on screen
@@ -465,7 +426,8 @@ window.addEventListener("DOMContentLoaded", function()
 			}
 			e.preventDefault();
 			return false;
-		}else{
+		}else
+		{
 			//If everything is good, save the data
 			//Send key value that came from editData function
 			//Remember key value was passed thru editSubmit even listener 
@@ -474,42 +436,21 @@ window.addEventListener("DOMContentLoaded", function()
 		}
 	}
 	
-	
 	// Variable defaults
 	// store values of dropdown in array
 	var mediaGroups = ["-- Choose Project Type--", "ios", "android", "html5", "wordpress", "graphic", "author"],
 		mtopicValue,
-		errMsg = noDollarSign("errors");
+		errMsg = momo("errors");
 		
 	makeMediaTypes();
 	
 	// Set Link & Submit Click Events
-	var displayLink = noDollarSign("displayLink");
+	var displayLink = momo("displayLink");
 	displayLink.addEventListener("click", getData);
-	
-	var clearLink = noDollarSign("clear");
+	var clearLink = momo("clear");
 	clearLink.addEventListener("click", clearLocal);
 	
-	var displayIOSLink = noDollarSign("displayIOSLink");
-	displayIOSLink.addEventListener("click", getData);
-	
-	var displayAndroidLink = noDollarSign("displayAndroidLink");
-	displayAndroidLink.addEventListener("click", getData);
-	
-	var displayHtml5Link = noDollarSign("displayHtml5Link");
-	displayHtml5Link.addEventListener("click", getData);
-
-	var displayWordpressLink = noDollarSign("displayWordpressLink");
-	displayWordpressLink.addEventListener("click", getData);
-
-	var displayGraphicLink = noDollarSign("displayGraphicLink");
-	displayGraphicLink.addEventListener("click", getData);
-
-	var displayAuthorLink = noDollarSign("displayAuthorLink");
-	displayAuthorLink.addEventListener("click", getData);
-	
-	var save = noDollarSign("submit");
-	
+	var save = momo("submit");
 	//save.addEventListener("click", saveMedia);
 	save.addEventListener("click", validate);
 });
