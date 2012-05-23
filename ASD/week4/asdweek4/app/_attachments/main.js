92714e5567aba7cbcf1749116e211f2d0f348935
monigarr <monigarr@yahoo.com>
2012-05-23 18:39:31 -0400

    Listview of items from couch on Home Page.
    todo: make crud form work.

--- ASD/week4/asdweek4/app/_attachments/main.js
+++ ASD/week4/asdweek4/app/_attachments/main.js
@@ -6,49 +6,43 @@
 //    Due Thursday May 24th 2012
 //    main.js
 
+//	change key to _id to work with couch
+//  pass revision info and _id to couch to do a save
+//  page reload for jqm
+//	dont set global vars to pass form data
+//  CREATE	form must work
+// 	READ	must show data from couch
+// 	UPDATE	change id if using custom id. delete entire doc for update.
+//	DELETE
+	
 
 $('#home').live('pageinit', function()
 {
 	console.log("Home Page Loaded.");
-
-	$('#additem').live('pageinit', function()
+	
+	//app is name of this app see _id file
+	//courses is name of db objects
+	$.couch.db("asdproject").view("app/courses", 
 	{
-		console.log("Add Item Page Loaded");
-	}); //end additem pageinit
-
-	$('#search').live('pageinit', function()
-	{
-		console.log("Search Page Loaded");
-	}); //end search pageinit
-
-	$('#browse').live('pageinit', function()
-	{
-		console.log("Browse Page Loaded");
-	}); //end browse pageinit
-
-	$('#contact').live('pageinit', function()
-	{
-		console.log("Contact Page Loaded");
-	}); //end contact pageinit
-
-	$('#info').live('pageinit', function()
-	{
-		console.log("Info Page Loaded");
-	}); //end info pageinit
-
-	//show console log message when returning back to home page
-    // transition to page, using data from a form and reloading 
-    // the page from scratch
-    //use this way from now on, declare as var to hold function
-    //so jqmobile won't go all wild hareyish
-    var toChangePage = function (toPageId) {
-        $.mobile.changePage("#" + toPageId , {
-            type:"post",
-            data:$("form").serialize(),
-            reloadPage:true
-        });
-    };
-
+		cache: false,
+		success: function(data)
+		{
+			$('#homeItems').empty();
+			$.each(data.rows, function(index, value)
+			{
+				var item = (value.value || value.doc);
+				$('#homeItems').append(
+					$('<li>').append(
+						$('<a>')
+							.attr("href", "project.html?project=" + item.acronym)
+							.text(item.title)
+						)
+					);
+			});
+			$('#homeItems').listview();
+		}
+	});
+	
 	//LOGO TOP
 	$('#header').empty();
 	$(function()
@@ -62,29 +56,14 @@ $('#home').live('pageinit', function()
 	{
 		$('#footer').append('');
 	});
-
-	//app is name of this app see _id file
-	//programs is name of db objects
-	$.couch.db("asdproject").view("app/programs", 
-	{
-		cache: false,
-		success: function(data)
-		{
-			$('#homeItems').empty();
-			$.each(data.rows, function(index, value)
-			{
-				var item = (value.value || value.doc);
-				$('#homeItems').append(
-					$('<li>').append(
-						$('<a>')
-							.attr("href", "program.html?program=" + item.acronym)
-							.text(item.title)
-						)
-					);
-				});
-				$('#homeItems').listview();
-		}
-	});
+	
+    var toChangePage = function (toPageId) {
+        $.mobile.changePage("#" + toPageId , {
+            type:"post",
+            data:$("form").serialize(),
+            reloadPage:true
+        });
+    };
 
 	var urlVars = function()
 	{
@@ -95,18 +74,30 @@ $('#home').live('pageinit', function()
 		var urlValues = {};
 		for (var pair in urlPairs)
 		{
-			var keyValue = urlPairs[pair].split('=');
-			var key = decodeURIComponent(keyValue[0]);
-			var value = decodeURIComponent(keyValue[1]);
-			urlValues[key] = value;
+			var _idValue = urlPairs[pair].split('=');
+			var _id = decodeURIComponent(_idValue[0]);
+			var value = decodeURIComponent(_idValue[1]);
+			urlValues[_id] = value;
 		}
 		return urlValues;
-	};	
-	// Var Defaults
-	var projectType = ["-- Choose Project Type--", "ios", "android", "html5", "wordpress", "graphic", "author"],mtopicValue;
+	};
+
+	$('#search').live('pageinit', function()
+	{
+		console.log("Search Page Loaded");
+	}); //end search pageinit
+
+	$('#browse').live('pageinit', function()
+	{
+		console.log("Browse Page Loaded");
+	}); //end browse pageinit
+
+	$('#additem').live('pageinit', function()
+	{
+		console.log("Add Item Page Loaded");
+			// Var Defaults
 		errMsg = $("errors");
-		projectId = getUrlVars()["projectId"];
-		projectForm = $("projectForm");
+		form = $("form");
 		displayLink = $("displayLink");
 		clearLink = $("clear");
 		displayIOSLink = $("displayIOSLink");
@@ -117,7 +108,7 @@ $('#home').live('pageinit', function()
 		displayAuthorLink = $("displayAuthorLink");
 		save = $("submit");
 		
-		displayLink.on("click", getProjectJSON);
+		/*displayLink.on("click", getProjectJSON);
 		clearLink.on("click", clearLocal);
 		displayIOSLink.on("click", getProjectJSON);
 		displayAndroidLink.on("click", getProjectJSON);
@@ -125,106 +116,15 @@ $('#home').live('pageinit', function()
 		displayWordpressLink.on("click", getProjectJSON);
 		displayGraphicLink.on("click", getProjectJSON);
 		displayAuthorLink.on("click", getProjectJSON);
+		*/
 		save.on("click", saveProject);
 		//save.on("click", validate);
 		
-		// Project Types DropDown
-		var makeProjectTypes = function() 
-		{
-			$('<select></select>')
-				.appendTo('#select');
-			//populate with options
-			for(var i=0, j=projectType.length; i<j; i++) 
-			{
-				var optText = projectType[i];
-				$('<option></option>')
-					.appendTo('select #mtype');
-				$('#mtype option:last-child')
-					.html(optText);
-			}
-			var selectType = $('select #mtype');
-			selectType.selectmenu();
-			selectType.selectmenu('refresh');
-		}
-		
-		// Find value of Selected Radio Button
-		var getSelectedRadio = function()
-		{
-			//create radio array
-			//var radios = document.forms[0].mtopics;
-			var radios = $("#mtopics").val();
-			for(var i=0; i<radios.length; i++)
-			{
-				if(radios[i].checked)
-				{
-					mtopicValue = radios[i].value;
-				}
-			}
-		}	
-		
-		var setDate = function()
-		{
-			if (!($('#date').val()))
-			{
-				var today = new Date();
-				var dd = today.getDate();
-				var mm = today.getMonth()+1;
-				var yyyy = today.getFullYear();
-					if(dd<10){dd='0'+dd;}
-					if(mm<10){mm='0'+mm;}
-					$('#date').val(mm+'/'+dd+'/'+yyyy);
-			}
-		}
-		
-		//Auto Populate local storage
-		var autoFillData = function()
-		{
-			//actual JSON Object data is coming from json.js file.
-			//json.js file is loaded from additem.html
-			//Store JSON Object into local storage
-			for(var n in json)
-			{
-				var id = Math.floor(Math.random()*10000001);
-				localStorage.setItem(id, JSON.stringify(json[n]));
-			}
-			$('#getProjectData').hide();
-			getProjectJSON();
-		}
-		
-		//Get icon for the relevant project type displayed
-		var getImage = function(mediaType, makeSubList)
-		{
-			var imageLi = $('div')
-				.attr('align', 'left')	
-				.append('#items li a' + key);
-			
-			var newImg = $('img')
-				.attr("src", "images/" + mediaType + ".jpg")
-				.attr('class', 'projectIconAlign');
-				.append('#imageLi' + key);
-			
-			
-		}
-		
-		//Get graphic url for project.
-		var getProjectGraphic = function(projectName, makeSubList)
-		{
-			var projectGraphicLi = $("div");
-			makeSubList.append(projectGraphicLi);
-			var newImg = $("img");
-			var setSrc = newImg.attr("src", projectName);
-			var alignImg = newImg.attr("class", "projectScreenshotAlign");
-			var setWidth = newImg.attr("width", "75px");
-			var setHeight = newImg.attr("height", "75px");
-			projectGraphicLi.append(newImg);
-			
-		}
-		
-		//Get youtube video review url for project.
+
 		
 		//Make Item Links
 		//Create Edit and Delete links for each stored item when displayed
-		var makeItemLinks = function(key, linksLi)
+		var makeItemLinks = function(_id, linksLi)
 		{
 			//create line break to create space 
 			//around elements
@@ -233,7 +133,7 @@ $('#home').live('pageinit', function()
 			//add edit single item link
 			var editLink = document.createElement("a");
 			editLink.href = "#";
-			editLink.key = key;
+			editLink._id = _id;
 			var editText = "Edit Project";
 			editLink.on("click", editItem);
 			editLink.html = editText;	
@@ -246,7 +146,7 @@ $('#home').live('pageinit', function()
 			//add delete single item link
 			var deleteLink = document.createElement("a");
 			deleteLink.href = "#";
-			deleteLink.key = key;
+			deleteLink._id = _id;
 			var deleteText = "Delete Project";
 			deleteLink.on("click", deleteProject);
 			deleteLink.html = deleteText;
@@ -262,48 +162,17 @@ $('#home').live('pageinit', function()
 			
 			//populate form fields with current local storage values
 			//1 is value, 0 is label
-			$('#mtype').val() = item.mtype[1];
-			$('#mdate').val() = item.mdate[1];
-			$('#mname').val() = item.mname[1];
-			$('#mrating').val() = item.mrating[1];
-			$('#mtags').val() = item.mtags[1];
-			$('#mgraphic').val() = item.mgraphic[1];
-			$('#mcomments').val() = item.mcomments[1];
-			
-			$('radio').removeAttr('checked');
-			var radios = document.forms[0].mtopics;
-			for(var i=0; i<radios.length; i++)
-			{
-				if(radios(i).value === "school" && item.mtopics(1) === "school")
-				{
-					$(radios[i]).attr("checked", "checked");
-				}
-				else if(radios(i).value === "work" && item.mtopics(1) === "work")
-				{
-					radios(i).attr("checked", "checked");
-				}
-				else if(radios(i).value === "inspiration" && item.mtopics(1) === "inspiration")
-				{
-					radios(i).attr("checked", "checked");
-				}
-			}
-			$("input[type='radio']:first")
-				.attr("checked",true)
-				.checkboxradio("refresh");
-			/*
-			// handle yes / no check box
-			if(obj.favorite(1) == "Yes")
-			{
-				$("fav").attr("checked", "checked");
-			}
-			*/
+			$('#title').val() = item.title[1];
+			$('#acronym').val() = item.acronym[1];
+			$('#month').val() = item.month[1];
+		
 			// Remove the initial listener from the input 'save project' button
 			$("#submit").unbind("click");
 			
 			// Change Submit button value to say Edit Button
 			$('submit').val('Edit Project');
 			$("submit").button('refresh');
-			$('#projectForm').submit(function()
+			$('#form').submit(function()
 			{
 				validate(projectId);
 			});
@@ -336,53 +205,30 @@ $('#home').live('pageinit', function()
 				alert("All Projects Deleted from local storage.");
 				window.location.reload();
 				return false;
-				//populate with test data
-				//autoFillData();
 			}
 		}
-				
 		var validate = function(projectId)
 		{
 			//Define elements we want to check
-			var getMtype = $("mtype").val();
-			var getMname = $("mname").val();
-			var getMdate = $("mdate").val();
+			var getTitle = $("title").val();
 			
 			//Reset error messages
 			$(".error").hide();
 			var hasError = false;
 			$('#errors').empty();
-			$('#mtype').css("border", "none");
-			$('#mname').css("border", "none");
-			$('#mdate').css("border", "none");
+			$('#title').css("border", "none");
 			
 			//Get error messages
 			var messageAry = [];
 			
-			// Project Type Validation
-			if(getMtype.value === "-- Choose Project Type--")
-			{
-				$('#select > div').after('<span class="error">Enter Project Type</span>');
-				var projectTypeError = "Choose Project Type";
-				$('#select > div').css("border", "1px solid yellow");
-				hasError = true;
-			}
-			
 			// Project Name Validation
-			if(getMname.value === "")
+			if(getTitle.value === "")
 			{
-				$('#mname').after('<span class="error">Enter Project Name</span>');
-				$('#mname').css("border", "1px solid yellow");
+				$('#title').after('<span class="error">Enter Project Name</span>');
+				$('#title').css("border", "1px solid yellow");
 				hasError = true;
 			}
 			
-			// Project Date Validation
-			if(getMdate.value === "")
-			{
-				$('#mdate').after('<span class="error">Enter Project Date</span>');
-				$('#mdate').css("border", "1px solid yellow");
-				hasError = true;
-			}
 			
 			// Set Errors
 			if (hasError === true)
@@ -412,111 +258,46 @@ $('#home').live('pageinit', function()
 				//Send key value that came from editData function
 				//Remember key value was passed thru editSubmit even listener 
 				//as a property.
-				saveProject(this.key);
+				//CHANGE KEY TO _id because couch db
+				saveProject(this._id);
 			}
 		}
 
-		var saveProject = function(key)
+		var saveProject = function(_id)
 		{
 			//if no key, this is brand new item 
 			//so we need new key
-			if(!key)
+			if(!_id)
 			{
 				var id = Math.floor(Math.random()*10000001);
 			}
-			//Remove Weird Data that creates keys for file directories
-			else if(key === "A-Z" || "a-z")
-			{
-				//delete weird data and move on
-				localStorage.removeItem(this.key);
-			}
 			else
 			{
-				var id = key;
+				var id = _id;
 			}
-			
-			// run function to find Selected Radio Button
-			getSelectedRadio();
-				
+
 				//Gather up all our form field values and store in object.
 				//Object properties contain array with form label and input value
 				var item 			= {};
-					item.mtype 		= ["Project Type:",$('#mtype').val()];
-					item.mgraphic   = ["Project Screenshot:",$('#mgraphic').val()];
-					item.mname 		= ["Project Name:",$('#mname').val()];
-					item.mdate  	= ["Project Date:",$('#mdate').val()];
-					item.mrating 	= ["Project Rating:",$('#mrating').val()];
-					//radio button
-					item.mtopics 	= ["Project Incentive:",$('#mtopicValue').val()];
-					item.mtags		= ["Project Tags:",$('#mtags').val()];
-					item.mcomments	= ["Project Notes:",$('#mcomments').val()];
+					item.title 		= ["Project Name:",$('#title').val()];
+					item.acronym		= ["Project Tags:",$('#acronym').val()];
+					item.month	= ["Project Notes:",$('#month').val()];
 				//Save Data to Local Storage: Use Stringify to convert our object to a string
 				//json.org
 				localStorage.setItem(id, JSON.stringify(item));
 				alert("Project Saved");
 		}
-		
-		$.get("data.xml", function(d)
-			{
-				$("body".append("<h2>Projects</h2>");
-				$("body").append("dl />");
-				$(d).find("item").each(function()
-				{
-					var $item = $(this);
-					var type = $item.attr("type");
-					var name = $item.find("name").text();
-					var comments = $item.find("comments").text();
-					var imageurl = $item.attr("graphic");
-					var html = '<dt><img class="projectImage" alt="" src="' + graphic + '" /></dt>';
-					html += '<dd><span class="loadingPic" alt="Loading" />';
-					html += '<p class="type">' + type + '</p>';
-					html += '<p>' + name + '</p>';
-					html += '<p>' + comments + '</p>';
-					html += '</dd>';
-					$("dl").append($(html));
-				});
-			});
-		});
 
 		var getData = function()
 		{
-			
-			if(localStorage.length === 0)
-			{
-				$('loadjson').show();
-				$('#loadjson').click(function()
-				{
-					alert("No Projects in local Storage. Test Data was Added.");
-					//populate with test data
-					autoFillData();
-					return false;
-				});
-			}
-							
-			for(var i=0, len=localStorage.length; i<len; i++)
-			{
-				var key = localStorage.key(i);
-				var value = localStorage.getItem(key);
-				var obj = JSON.parse(value);
-				
-				$('<li></li>').appendTo('#items');
-				
-				$('<a></a>').appendTo('#items li.' + key);
-				
-				for(var n in obj)
-				{
-					//0 is label, 1 is the value
-					//var optSubText = obj[n][0] + " " + obj[n][1];
-					var optSubText = obj[n][1];
-					makeSubli.html = optSubText;
-					makeSubli.append(linksLi);
-				}
-				//add edit and delete button from function
-				//for each item in local storage.
-				makeItemLinks(localStorage.key(i), linksLi);
-			}
+			//removed localStorage stuff
 		}
-		
-		makeProjectTypes();
-		setDate();
+	}); //end additem pageinit		
+			
+	$('#contact').live('pageinit', function()
+	{console.log("Contact Page Loaded");}); //end contact pageinit
+
+	$('#info').live('pageinit', function()
+	{console.log("Info Page Loaded");}); //end info pageinit
+
 }); //end home page init
--- ASD/week4/asdweek4/app/views/courses/map.js
+++ ASD/week4/asdweek4/app/views/courses/map.js
@@ -3,9 +3,9 @@ function(doc) {
 //loop over docs & call function
   if (doc._id.substr(0, 7) === "course:") {
     emit(doc._id.substr(7), {
-    	"title": doc.title,
-    	"program": doc.program,
-    	"months": doc.month
+     "title": doc.title,
+     "acronym": doc.acronym,
+     "months": doc.months
     });
   }
 };
